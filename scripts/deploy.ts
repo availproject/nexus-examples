2caab471f178f4b633d8c64d92c219dd8a72c4b3
rac-sri <rac.sri25@gmail.com>
2024-05-23 19:40:43 +0530

    chore: add second library in deployment pipeline

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,9 @@
-// scripts/deploy.js
-
 import { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
-  // Deploy the library first
   const LibSecureMerkleTrie = await ethers.getContractFactory(
     "Lib_SecureMerkleTrie"
   );
@@ -14,10 +11,15 @@ async function main() {
 
   console.log("Library deployed to:", await libSecureMerkleTrie.getAddress());
 
-  // Link the library to the contract
+  const LibRLPReader = await ethers.getContractFactory("Lib_RLPReader");
+  const libRLPReader = await LibRLPReader.deploy();
+
+  console.log("Library deployed to:", await libRLPReader.getAddress());
+
   const StorageVerifier = await ethers.getContractFactory("StorageVerifier", {
     libraries: {
       Lib_SecureMerkleTrie: await libSecureMerkleTrie.getAddress(),
+      Lib_RLPReader: await libRLPReader.getAddress(),
     },
   });
   const storageVerifier = await StorageVerifier.deploy();
