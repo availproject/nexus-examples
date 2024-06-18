d357d8ba781e5fd04490afea7b849b4a2817020b
rac-sri <rac.sri25@gmail.com>
2024-06-19 04:59:08 +0530

    chore: update bridge script with new interface

--- off-chain/src/bridge.rs
+++ off-chain/src/bridge.rs
@@ -1,5 +1,5 @@
-use abi::AbiEncode;
 use anyhow::Result;
+use console::{style, StyledObject};
 use ethers::abi::encode;
 use ethers::core::abi::Abi;
 use ethers::{
@@ -7,18 +7,11 @@ use ethers::{
     signers::{LocalWallet, MnemonicBuilder},
     utils::*,
 };
-use hex_literal::hex;
-
-use console::{style, StyledObject};
-use indicatif::{ProgressBar, ProgressStyle};
-use macros::ethers_core_crate;
+use reqwest::blocking::Client;
 use rlp::RlpStream;
-use sha3::digest::consts::U25;
-use std::fmt::Pointer;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
-use std::ops::Deref;
-use std::str::FromStr;
 use std::{sync::Arc, vec};
 
 // fill these after deployment
@@ -34,20 +27,39 @@ pub struct Proof {
     block_number: U64,
     state_hash: H256,
     address: H160,
+    storage_slot: [u8; 32],
 }
 
-const TICK_MARK: &str = "✓";
-const CROSS_MARK: &str = "x";
-// #[derive(Clone, Debug, Default, EthAbiType)]
-// pub struct Message {
-//     pub message_type: [u8; 1],
-//     pub from: [u8; 32],
-//     pub to: [u8; 32],
-//     pub origin_domain: u32,
-//     pub destination_domain: u32,
-//     pub data: Vec<u8>,
-//     pub message_id: u64,
-// }
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct StatementDigest(pub [u32; 8]);
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct AccountState {
+    pub statement: StatementDigest,
+    pub state_root: [u8; 32],
+    pub start_nexus_hash: [u8; 32],
+    pub last_proof_height: u32,
+    pub height: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProofResponse {
+    leaves_bitmap: Vec<Vec<u8>>,
+    merkle_path: Vec<String>, // Assuming merkle_path is an array of strings, adjust type if needed
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RpcResponse {
+    account: AccountState,
+    proof: ProofResponse,
+}
+
+const TICK_MARK_SYMBOL: &str = "✓";
+const CROSS_MARK_SYMBOL: &str = "x";
+
+static TICK_MARK: StyledObject<&str> = style(TICK_MARK_SYMBOL).green().bold();
+static CROSS_MARK: StyledObject<&str> = style(CROSS_MARK_SYMBOL).red().bold();
+
 pub async fn crosschain_wrapper() -> Result<()> {
     let private_key = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
     let wallet = private_key.parse::<LocalWallet>()?;
@@ -88,9 +100,6 @@ async fn crosschain(
 ) -> Result<()> {
     // assuming deployment of StorageProof contract is already done
 
-    let tick_mark = style(TICK_MARK).green().bold();
-    let cross_mark = style(CROSS_MARK).red().bold();
-
     let address = bridge_address.parse::<Address>()?;
 
     let abi = read_abi_from_file("./bridge.json").unwrap();
@@ -109,121 +118,49 @@ async fn crosschain(
 
             let encoded_proof = format!("0x{}", encode_proof(storage_proof.proof.account_proof));
 
-            let method_call = state_manager_destination
-                .method::<(U256, U256, U256, Bytes, H256), ()>(
-                    "updateChainState",
-                    (
-                        U256::from(137),
-                        U256::from(storage_proof.block_number.as_u64()),
-                        U256::from(1),
-                        hex_to_bytes(&encoded_proof.as_str())?,
-                        storage_proof.state_hash,
-                    ),
-                )?;
+            let base_url = "http://localhost:7000/accounts";
+            let account_app_id = "<hash>"; // Replace <hash> with the actual hash value
+
+            let client = Client::new();
+
+            let response = client
+                .get(base_url)
+                .query(&[("account_app_id", account_app_id)])
+                .send()?;
+            let json_response = response.text()?;
+
+            let rpc_response: RpcResponse = serde_json::from_str(json_response.as_str())?;
+            // fetch account proof from nexus
+            let method_call = state_manager_destination.update_chain_state(
+                U256::from(11),
+                rpc_response.proof.merkle_path.clone(),
+                account_app_id,
+                rpc_response.account.clone(),
+            );
 
             let result = method_call.send().await;
 
             match result {
                 Ok(tx) => {
-                    println!("{} {}", tick_mark,style("Successfully updated the origin chain state on destination chain using nexus proof").green().italic());
+                    println!("{} {}", TICK_MARK,style("Successfully updated the origin chain state on destination chain using nexus proof").green().italic());
                     let finalisation = tx.confirmations(1).await?;
                     match finalisation {
                         Some(_) => {
-                            println!(
-                                "{}",
-                                style("Now minting by calling receiveEth on destination chain")
-                                    .green()
+                            update_nexus_state_root_and_bridge(
+                                rpc_provider_destination,
+                                encoded_proof,
+                                storage_proof,
                             );
-
-                            let bridge_contract_destination = BridgeContract::new(
-                                BRIDGE_ADDRESS_138.parse::<Address>()?,
-                                rpc_provider_destination.clone(),
-                            );
-
-                            /************************************* */
-                            // mock a send transaction on destination contract to fund it with eth
-                            let address_str = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
-                            let address_bytes = hex::decode(address_str).expect("Decoding failed");
-                            let address_array: [u8; 20] =
-                                address_bytes.as_slice().try_into().expect("Invalid length");
-                            let mut padded_address = [0u8; 32];
-
-                            padded_address[12..].copy_from_slice(&address_array);
-                            let amount_to_send: U256 = U256::from(1_000_000_000_000_000_000_u64);
-
-                            let provider = Provider::<Http>::try_from("http://localhost:8546")?;
-                            let block_n = provider.get_block_number().await?;
-
-                            let send_tx = bridge_contract_destination
-                                .send_eth(padded_address)
-                                .value(amount_to_send);
-                            let _ = send_tx.send().await;
-
-                            //******************************** */
-                            let value1 = abi::Token::String(
-                                "4554480000000000000000000000000000000000000000000000000000000000"
-                                    .to_string(),
-                            );
-                            let value2 = abi::Token::Uint(1.into());
-                            let encoded = encode(&[value1, value2]);
-
-                            // Check if state root is updated correctly - done
-
-                            let message = Message {
-                                message_type: [0x02],
-                                from: hex::decode("00000000000000000000000070997970C51812dc3A010C7d01b50e0d17dc79C8")?.try_into().unwrap(),
-                                to: hex::decode("0000000000000000000000003347B4d90ebe72BeFb30444C9966B2B990aE9FcB")?.try_into().unwrap(),
-                                origin_domain: 1_u32,
-                                destination_domain: 2_u32,
-                                data: Bytes::from(encoded),
-                                message_id: 2_u64,
-                            };
-
-                            let tx = bridge_contract_destination.receive_eth(
-                                message,
-                                Bytes::from(encoded_proof.as_bytes().to_vec()),
-                            );
-
-                            let receipt = tx.send().await;
-
-                            match receipt {
-                                Ok(rec) => {
-                                    println!("{} {}", tick_mark ,style("Successfully called recieve eth function to unlock funds on target chain").green().bold());
-                                    let rr = rec.await;
-
-                                    // TODO: remove the line below
-                                    println!("{:?}", rr);
-                                }
-                                Err(err) => {
-                                    let error_string = format!("{:?}", err);
-
-                                    if error_string.contains("Revert") {
-                                        println!(
-                                            "{} {} {:?}",
-                                            cross_mark,
-                                            style("Transaction reverted with data:").red(),
-                                            err
-                                        );
-                                    } else {
-                                        println!(
-                                            "{} {} {:?}",
-                                            cross_mark,
-                                            style("Transaction failed: ").red(),
-                                            err
-                                        );
-                                    }
-                                }
-                            }
                         }
                         None => {
-                            eprint!("{} {}", cross_mark, style("Error - invalid tx").red());
+                            eprint!("{} {}", CROSS_MARK, style("Error - invalid tx").red());
                         }
                     }
                 }
                 Err(e) => {
                     eprintln!(
                         "{} {} {:?}",
-                        cross_mark,
+                        CROSS_MARK,
                         style("Error sending transaction: ").red(),
                         e
                     );
@@ -231,13 +168,115 @@ async fn crosschain(
             }
         }
         Err(e) => {
-            eprint!("{} {} {:?}", cross_mark, style("error: ").red(), e);
+            eprint!("{} {} {:?}", CROSS_MARK, style("error: ").red(), e);
         }
     }
 
     Ok(())
 }
 
+async fn update_nexus_state_root_and_bridge(
+    rpc_provider_destination: Arc<SignerMiddleware<Provider<Http>, LocalWallet>>,
+    encoded_proof: String,
+    proof: Proof,
+) -> Result<()> {
+    println!(
+        "{}",
+        style("Now minting by calling receiveEth on destination chain").green()
+    );
+
+    let bridge_contract_destination = BridgeContract::new(
+        BRIDGE_ADDRESS_138.parse::<Address>()?,
+        rpc_provider_destination.clone(),
+    );
+
+    mock_send_transaction_on_destination_to_fund_contract(bridge_contract_destination);
+
+    let value1 = abi::Token::String(
+        "4554480000000000000000000000000000000000000000000000000000000000".to_string(),
+    );
+    let value2 = abi::Token::Uint(1.into());
+    let encoded = encode(&[value1, value2]);
+
+    // TODO: fix typo
+    let message = MessageReceieve {
+        message_type: [0x02],
+        from: hex::decode("00000000000000000000000070997970C51812dc3A010C7d01b50e0d17dc79C8")?
+            .try_into()
+            .unwrap(),
+        to: hex::decode("0000000000000000000000003347B4d90ebe72BeFb30444C9966B2B990aE9FcB")?
+            .try_into()
+            .unwrap(),
+        origin_domain: 1_u32,
+        destination_domain: 2_u32,
+        data: Bytes::from(encoded),
+        message_id: 2_u64,
+        storage_proof: proof.proof.storage_proof,
+        storage_slot: proof.storage_slot,
+    };
+
+    let tx = bridge_contract_destination
+        .receive_eth(message, Bytes::from(encoded_proof.as_bytes().to_vec()));
+
+    let receipt = tx.send().await;
+
+    match receipt {
+        Ok(rec) => {
+            println!(
+                "{} {}",
+                TICK_MARK,
+                style("Successfully called recieve eth function to unlock funds on target chain")
+                    .green()
+                    .bold()
+            );
+            let rr = rec.await;
+
+            // TODO: remove the line below
+            println!("{:?}", rr);
+        }
+        Err(err) => {
+            let error_string = format!("{:?}", err);
+
+            if error_string.contains("Revert") {
+                println!(
+                    "{} {} {:?}",
+                    CROSS_MARK,
+                    style("Transaction reverted with data:").red(),
+                    err
+                );
+            } else {
+                println!(
+                    "{} {} {:?}",
+                    CROSS_MARK,
+                    style("Transaction failed: ").red(),
+                    err
+                );
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn mock_send_transaction_on_destination_to_fund_contract(
+    bridge_contract_destination: BridgeContract<SignerMiddleware<Provider<Http>, LocalWallet>>,
+) -> Result<()> {
+    let address_str = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+    let address_bytes = hex::decode(address_str).expect("Decoding failed");
+    let address_array: [u8; 20] = address_bytes.as_slice().try_into().expect("Invalid length");
+    let mut padded_address = [0u8; 32];
+
+    padded_address[12..].copy_from_slice(&address_array);
+    let amount_to_send: U256 = U256::from(1_000_000_000_000_000_000_u64);
+
+    let provider = Provider::<Http>::try_from("http://localhost:8546")?;
+    let block_n = provider.get_block_number().await?;
+
+    let send_tx = bridge_contract_destination
+        .send_eth(padded_address)
+        .value(amount_to_send);
+    let _ = send_tx.send().await;
+    Ok(())
+}
 fn encode_proof(account_proofs: Vec<Bytes>) -> String {
     let proofs = concatenate_proof(account_proofs);
     let rlp_encoded_proof = encode_rlp(proofs);
@@ -287,7 +326,7 @@ pub async fn send_eth_and_receive_proof(
         let block_number = receipt.block_number.unwrap();
         println!(
             "{} {} {:?}",
-            style(TICK_MARK).green().bold(),
+            TICK_MARK,
             style("Successfully called Send Eth on origin chain")
                 .green()
                 .italic(),
@@ -296,12 +335,19 @@ pub async fn send_eth_and_receive_proof(
         let block = provider.get_block(BlockNumber::Latest).await?.unwrap();
         let state_root = block.state_root;
 
-        let proof: EIP1186ProofResponse =
-            get_storage_slot(provider, contract.clone(), BlockId::from(block_number)).await?;
+        let storage_slot = get_storage_slot(contract.clone()).await?;
+
+        let proof: EIP1186ProofResponse = get_storage(
+            provider,
+            contract.clone(),
+            BlockId::from(block_number),
+            storage_slot,
+        )
+        .await?;
 
         println!(
             "{} {} {:?}",
-            style(TICK_MARK).green().bold(),
+            TICK_MARK,
             style("Successfully fetched the storage proof from origin chain")
                 .green()
                 .italic(),
@@ -313,6 +359,7 @@ pub async fn send_eth_and_receive_proof(
             block_number: block_number,
             state_hash: state_root,
             address: contract.address(),
+            storage_slot: storage_slot,
         };
 
         return Ok(proof_extended);
@@ -321,10 +368,8 @@ pub async fn send_eth_and_receive_proof(
 }
 
 async fn get_storage_slot(
-    provider: Arc<SignerMiddleware<Provider<Http>, LocalWallet>>,
     contract: Contract<Arc<SignerMiddleware<Provider<Http>, LocalWallet>>>,
-    block_number: BlockId,
-) -> Result<EIP1186ProofResponse> {
+) -> Result<[u8; 32]> {
     let key = contract.method::<_, U256>("messageId", ())?.call().await?;
     let slot = U256::zero();
     let mut encoded = [0u8; 64];
@@ -332,7 +377,14 @@ async fn get_storage_slot(
     slot.to_big_endian(&mut encoded[32..64]);
 
     let storage_slot = keccak256(&encoded);
-
+    Ok(storage_slot)
+}
+async fn get_storage(
+    provider: Arc<SignerMiddleware<Provider<Http>, LocalWallet>>,
+    contract: Contract<Arc<SignerMiddleware<Provider<Http>, LocalWallet>>>,
+    block_number: BlockId,
+    storage_slot: [u8; 32],
+) -> Result<EIP1186ProofResponse> {
     let address = contract.address();
     let proof = provider
         .get_proof(address, vec![H256::from(storage_slot)], Some(block_number))
